19f60356e426cde54fd0adb2626501a14fbc0a6e
DeadAnjali <122856861+DeadAnjali@users.noreply.github.com>
2023-03-18 18:03:05 +0530

    Add files via upload

---
+++ DEv jam/javas.js
@@ -0,0 +1,14 @@
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+
+
+
+
+  
