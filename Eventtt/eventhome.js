29433c5d86de66a48621b16a98988c878d4e94a0
DeadAnjali <122856861+DeadAnjali@users.noreply.github.com>
2023-03-20 17:20:47 +0530

    Add files via upload

---
+++ Eventtt/eventhome.js
@@ -0,0 +1,20 @@
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
